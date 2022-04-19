785c709adbb3a13ea3666dee6304772d9dd529fc
Angelo  Menti <angelo.omarzinho@outlook.com>
2022-04-18 23:07:59 -0300

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import AuthProvider from './context/authContext';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <Routes />
       </BrowserRouter>
